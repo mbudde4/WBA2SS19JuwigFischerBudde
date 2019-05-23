568a59d49c511e0d86908dfad88423430cc45906
Marjuw <33716855+Marjuw@users.noreply.github.com>
2019-05-23 17:29:21 +0200

    Update

--- MS2/api/routes/gerichte.js
+++ MS2/api/routes/gerichte.js
@@ -8,9 +8,59 @@ const router = express.Router();
 
 //Liste aller Gerichte
 router.get('/', (req, res, next) =>{
+
+    //Anwendung:EIngabe Array (an Lebensmittel) mit Lebensmittel in rezepte.Json vergleichen
+
+    var anz_lebensmittel = ["Eier", "Milch", "Butter","Spinat","Kartoffeln"]; 
+  //  var anz_2 = ["Eier", "Milch", "Butter"];
+
+    'use strict';
+
+    const fs = require('fs');
+
+    let rawdata = fs.readFileSync('rezepte.json');
+    let rezepte = JSON.parse(rawdata);
+
+
+    //console.log(rezepte.rezepte[0].inhalt);
+    //console.log(rezepte.rezepte[0].gericht);
+
+    
+    //console.log(anz_lebensmittel.toString().includes(anz_2.toString(),0));
+
+
+    var ausgabe=[100];    
+    var x=0;
+    var lehr=true;
+    
+    for(var i=0; i<rezepte.rezepte.length;i++) {
+
+        if((anz_lebensmittel.toString().includes(rezepte.rezepte[i].inhalt.toString()))==true){
+
+            lehr=false;
+
+            console.log(rezepte.rezepte[i].gericht+"\n"+"Inahlt:  "+rezepte.rezepte[i].inhalt+"\n"+"Nährwerte:  "+rezepte.rezepte[i].nährwerte);
+            console.log("\n\n")
+
+            ausgabe[x]=rezepte.rezepte[i].gericht;
+            x++;
+            ausgabe[x]=rezepte.rezepte[i].inhalt;
+            x++
+            ausgabe[x]=rezepte.rezepte[i].nährwerte;
+            x++
+            ausgabe[x]="";
+            x++
+        }
+        
+        if (lehr==true){
+            ausgabe[0]="Keine passenden Gerichte";
+        }
+    }
+
     res.status(200).json({
-        message: 'Handling GET requests to /gerichte'
+        gericht: ausgabe
     });
+
 });
 
 //Einzelnes Gericht
--- MS2/app.js
+++ MS2/app.js
@@ -6,6 +6,8 @@ const gerichteRoutes = require ('./api/routes/gerichte');
 const lebensmittelRoutes = require ('./api/routes/lebensmittel');
 const verfuegbareLebensmittelRoutes = require ('./api/routes/verfuegbareLebensmittel');
 
+
+
 //ist dafür da um Feedback in der Konsole zu bekommen was der Server gerade macht und wie lange es geauert hat. Z.B. GET Anfrage
 app.use(morgan('dev'));
 
@@ -14,6 +16,8 @@ app.use(morgan('dev'));
 app.use('/gerichte', gerichteRoutes);
 app.use('/lebensmittel', lebensmittelRoutes);
 app.use('/verfuegbareLebensmittel', verfuegbareLebensmittelRoutes);
+
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
@@ -30,5 +34,41 @@ app.use((error, req, res, next) => {
     });
 });
 
+/*
+//Anwendung:EIngabe Array (an Lebensmittel) mit Lebensmittel in rezepte.Json vergleichen
 
+var anz_lebensmittel = ["Eier", "Milch", "Butter"]; 
+var anz_2 = ["Eier", "Milch", "Butter"];
+
+'use strict';
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('rezepte.json');
+let rezepte = JSON.parse(rawdata);
+
+
+//console.log(rezepte.rezepte[0].inhalt);
+//console.log(rezepte.rezepte[0].gericht);
+
+
+
+for(var i=0; i<rezepte.rezepte.length;i++) {
+
+    //console.log(anz_lebensmittel.toString() == rezepte.rezepte[i].inhalt.toString())
+  if((anz_lebensmittel.toString() == rezepte.rezepte[i].inhalt.toString())==true){
+
+     console.log(rezepte.rezepte[i].gericht+"\n"+"Inahlt:  "+rezepte.rezepte[i].inhalt+"\n"+"Nährwerte:  "+rezepte.rezepte[i].nährwerte);
+
+  }
+
+}
+
+*/
 module.exports = app;
+
+
+
+
+
+
