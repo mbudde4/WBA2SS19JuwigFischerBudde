26fef2f1cec50780b535d1a8874edb38af72fcae
Marc-Andre Budde (inf544) <marc-andre.budde@smail.th-koeln.de>
2019-05-23 19:12:44 +0200

    VerfuegbareLebensmittel-POST
    Der Post funktioniert soweit, dass die Lebensmittel in einem JSON-file abgelegt werden

--- MS2/api/routes/verfuegbareLebensmittel.js
+++ MS2/api/routes/verfuegbareLebensmittel.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var	fs=require('fs');
 
 router.get('/', (req, res, next) =>{
     res.status(200).json({
@@ -7,10 +8,31 @@ router.get('/', (req, res, next) =>{
     });
 });
 
+
+
 router.post('/', (req, res, next) =>{
+    
+    //var verfuegbareLebensmittel = req.query.lebensmittel;
+    
+    //var string = JSON.stringify(obj);
+    var string = {verfuegbareLebensmittel : req.query.lebensmittel};
+    
+    string =  JSON.stringify(string);
+
+
+    fs.writeFile("lebensmittelliste.json", string, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+
     res.status(201).json({
         message: 'Lebensmittel was created'
     });
+
 });
+
+
 
 module.exports = router;
